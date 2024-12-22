c7ca42e1687d4940a63ec16e7bc8f56c2143ba74
SAwandya <sachilaawandya@gmail.com>
2024-12-22 09:13:47 +0530

    create database connection

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const session = require("express-session");
+
+const mongo_url = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongo_url)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
+
+app.use(cors());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(
+  session({
+    secret: "Awandya2000#",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: process.env.NODE_ENV === "production" },
+  })
+);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
